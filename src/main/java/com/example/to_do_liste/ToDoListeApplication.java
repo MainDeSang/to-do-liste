1cd5e268ebc0c2cef9ea41a06cf57251eb9e7eb6
johann <johann.lindele@tn.mischok.academy>
2025-04-07 14:26:15 +0200

    Spring initialized ToDo Project

---
+++ src/main/java/com/example/to_do_liste/ToDoListeApplication.java
@@ -0,0 +1,13 @@
+package com.example.to_do_liste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListeApplication.class, args);
+	}
+
+}
