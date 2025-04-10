c6789ba01b38bc93bcd32a2e856aea8f720621df
Johann A. K.  Lindele <johann.lindele@tn.mischok.academy>
2025-04-10 16:41:54 +0200

    ProjectMapper als interface implementiert
    MapStruct Plugin für @Mapper Annotationen hinzugefügt (erspart "Mapper Boilerplatecode" = hart reingecoded)

--- src/main/java/com/example/to_do_liste/mapper/ProjectMapper.java
+++ src/main/java/com/example/to_do_liste/mapper/ProjectMapper.java
@@ -1,7 +1,16 @@
 package com.example.to_do_liste.mapper;
 
+import com.example.to_do_liste.dto.ProjectDTO;
+import com.example.to_do_liste.entity.Project;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
+//Mappt zwei Objekte mithilfe von MAPSTRUCT Plugin
 @Mapper
-public class ProjectMapper {
+public interface ProjectMapper {
+    ProjectMapper INSTANCE = Mappers.getMapper( ProjectMapper.class);
+
+    @Mapping(source = "numberOfSeats", target = "seatCount")
+    ProjectDTO projectToProjectDTO(Project project);
 }
