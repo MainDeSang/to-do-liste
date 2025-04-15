cae5b672feb8f8a9839a87eb78a92cd5bd90d5d1
oliverkirsch <oliver.kirsch@tn.mischok.academy>
2025-04-15 10:19:56 +0200

    dto LoginRequest aufgeräumt. einen debug controller erstellt zur abfrage aller einträge in person tabelle

--- src/main/java/com/example/to_do_liste/dto/LoginRequest.java
+++ src/main/java/com/example/to_do_liste/dto/LoginRequest.java
@@ -1,24 +1,13 @@
 package com.example.to_do_liste.dto;
 
+import lombok.Getter;
+
+@Getter
 public class LoginRequest {
+    // Getter und Setter
     private String email;
     private String password;
 
-    // Getter und Setter
-    public String getEmail() {
-        return email;
-    }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
 
