4ed6b5202b19363d1197dcb2cdb2c391a1760362
Johann <johann.lindele@tn.mischok.academy>
2025-04-17 13:57:54 +0200

    ProjectController.java um neue create & update methode erweitert. ProjectService.java create & update implementiert

--- src/main/java/com/example/to_do_liste/service/ProjectService.java
+++ src/main/java/com/example/to_do_liste/service/ProjectService.java
@@ -1,16 +1,13 @@
 package com.example.to_do_liste.service;
 
-import com.example.to_do_liste.dto.TodoDto;
 import com.example.to_do_liste.model.Person;
 import com.example.to_do_liste.model.Project;
 import com.example.to_do_liste.model.Todo;
 import com.example.to_do_liste.repository.PersonRepository;
 import com.example.to_do_liste.repository.ProjectRepository;
-import com.example.to_do_liste.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -18,8 +15,6 @@ public class ProjectService {
 
     private final ProjectRepository projectRepository;
     private final PersonRepository personRepository;
-    private final TodoRepository todoRepository;
-    private final TodoService todoService;
 
     public Project createProject(Project project) {
         // Prüfung ob Title vergeben wurde, andernfalls wird eine Fehlermeldung geworfen
@@ -38,9 +33,7 @@ public class ProjectService {
         Project updatedProject = projectRepository.findById(project.getId())
                 .orElseThrow(
                         () -> new RuntimeException("Kein Projekt gefunden"));
-        // Besitzer des Projektes aus der Datenbank holen
-        Person owner = personRepository.findById(project.getOwner().getId())
-                .orElseThrow(() -> new RuntimeException("Person nicht gefunden"));
+
 
         // Felder aktualisieren
         project.setTitle(updatedProject.getTitle());
